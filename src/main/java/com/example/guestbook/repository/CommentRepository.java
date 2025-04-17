d7e86935d127746813c5514c8b462cb2d1afbbe4
SHIN SUJUNG <shinssjj79@gmail.com>
2025-04-17 21:02:59 +0900

    Add CommentRepository interface for managing Comment entities

---
+++ src/main/java/com/example/guestbook/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.guestbook.repository;
+
+import com.example.guestbook.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
